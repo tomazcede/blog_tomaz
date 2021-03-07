ab95276d48238a3de31a7ee23936a2363f4e239c
Tomaz Cede <tomaz.cede@scv.si>
2021-03-07 12:33:25 +0100

    Error z boostrap

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,20 @@
+module PostsHelper
+	def display_post_index_links(post)
+		html = "<td>#{link_to 'Show', post}</td>"
+		if current_user == post.user
+	        html += "<td>#{link_to 'Edit', edit_post_path(post)}</td>"
+	        html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
+	    end
+        html.html_safe
+	end
+
+	def display_post_show_links(post)
+		html = ""
+		if current_user == post.user
+			html += link_to 'Edit', edit_post_path(post)
+			html += ' | '
+		end
+		html += link_to 'Back', posts_path
+		html.html_safe
+	end
+end
